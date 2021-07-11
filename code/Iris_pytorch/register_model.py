3233b40ba703ca36fc39b62377080f4ae001328d
Anh Le <anle@suncor.com>
2021-07-11 15:39:08 -0600

    update 10

--- code/Iris_pytorch/register_model.py
+++ code/Iris_pytorch/register_model.py
@@ -56,10 +56,63 @@ ws = Workspace.from_config(
     _file_name=config_file_name)
 print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep='\n')
 
+
+# -----------mlflow----------------
+# Load the JSON settings file and relevant section
+print("Loading settings")
+with open(os.path.join("code", "settings.json")) as f:
+    settings = json.load(f)
+experiment_settings = settings["experiment"]
+deployment_settings = settings["deployment"]
+
+os.environ["MLFLOW_TRACKING_URI"] = ws.get_mlflow_tracking_uri()
+
+# Attach Experiment
+print("Loading Experiment")
+exp = Experiment(workspace=ws, name=experiment_settings["name"])
+mlflow.set_experiment(exp.name)
+print(exp.name, exp.workspace.name, sep="\n")
+
+# -------------------------------way 1--------------------------------
+'''
+# Use MLflow to build a Container Image for the trained model
+
+# Use the `mlflow.azuereml.build_image` function to build an Azure Container Image for the trained MLflow model. 
+# This function also registers the MLflow model with a specified Azure ML workspace. 
+# The resulting image can be deployed to Azure Container Instances (ACI) or Azure Kubernetes Service (AKS) for real-time serving.
+
+
+
+model_image, azure_model = mlflow.azureml.build_image(model_uri='runs:/{}/{}'.format(run_details["run_id"], deployment_settings["model"]["path"]),
+                                                      workspace=ws,
+                                                      model_name="model",
+                                                      image_name="model",
+                                                      description="Sklearn image for predicting iris type",
+                                                      synchronous=False)
+model_image.wait_for_creation(show_output=True)
+
+
+# Create an ACI webservice deployment
+
+# The [ACI platform](https://docs.microsoft.com/en-us/azure/container-instances/) is the recommended environment for staging and developmental model deployments. 
+# Using the Azure ML SDK, deploy the Container Image for the trained MLflow model to ACI.
+
+
+dev_webservice_name = "iris-model"
+dev_webservice_deployment_config = AciWebservice.deploy_configuration()
+dev_webservice = Webservice.deploy_from_image(
+    name=dev_webservice_name, image=model_image, deployment_config=dev_webservice_deployment_config, workspace=ws)
+dev_webservice.wait_for_deployment()
+
+'''
+# =============================way 2===========================================================
+
+
 # Loading Run
 print("Loading Run")
 experiment = Experiment(workspace=ws, name=run_details["experiment_name"])
 run = Run(experiment=experiment, run_id=run_details["run_id"])
+
 
 # Register model
 tags = deployment_settings["model"]["tags"]
